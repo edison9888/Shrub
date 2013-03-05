203bf7b52e527db574d3df826751bbf39308b7d0
Andrew Carter <andrew.r.carter@gmail.com>
2013-03-04 22:08:58 -0500

    Initial Commit

---
+++ Shrub/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  Shrub
+//
+//  Created by Andrew Carter on 3/4/13.
+//  Copyright (c) 2013 Pinch Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shrub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shrub
+//
+//  Created by Andrew Carter on 3/4/13.
+//  Copyright (c) 2013 Pinch Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAAppDelegate class]));
+    }
+}
